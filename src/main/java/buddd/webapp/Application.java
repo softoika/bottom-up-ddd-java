3ae1d82ea9cb61e7b2b946349d475dd4a873317b
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2019-10-30 10:07:52 +0900

    DIができていなかった問題を修正

--- src/main/java/buddd/webapp/Application.java
+++ src/main/java/buddd/webapp/Application.java
@@ -3,7 +3,7 @@ package buddd.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "buddd")
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
